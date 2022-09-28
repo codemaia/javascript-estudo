e6d89d496a6052a1b2c6f795b6e76ab552e3b90d
Renan Maia <renanmaia.rl@gmail.com>
2022-09-27 21:18:22 -0300

    if and else

--- app.js
+++ app.js
@@ -1,375 +1,418 @@
-/* WELCOME TO THE FANTASTIC WORLD OF JAVASCRIPT */
+// /* WELCOME TO THE FANTASTIC WORLD OF JAVASCRIPT */
 
-//console.log(2, 1);
+// //console.log(2, 1);
 
 
-// AULA 01-06 ****************************************************************************
+// // AULA 01-06 ****************************************************************************
 
-/*let age = 33;
-let currentYear = 2022;
+// /*let age = 33;
+// let currentYear = 2022;
 
-console.log(age, currentYear);
+// console.log(age, currentYear);
 
-age = 32;
+// age = 32;
 
-console.log(age);
+// console.log(age);
 
-const points = 100; //não aceita atribuição
-console.log(points);
+// const points = 100; //não aceita atribuição
+// console.log(points);
 
-*/
+// */
 
 
 
 
-//AULA 02-03 ****************************************************************************
-// STRINGS
+// //AULA 02-03 ****************************************************************************
+// // STRINGS
 
-/*
-const email = 'renanmaia@email.com';
-const firstName = 'Renan';
-const lastName = 'Maia';
-const fullName = firstName + ' ' + lastName;
+// /*
+// const email = 'renanmaia@email.com';
+// const firstName = 'Renan';
+// const lastName = 'Maia';
+// const fullName = firstName + ' ' + lastName;
 
-console.log(fullName);
+// console.log(fullName);
 
-// acessando caracteres
+// // acessando caracteres
 
-console.log(fullName[2]);
+// console.log(fullName[2]);
 
 
-// comprimento de uma string
+// // comprimento de uma string
 
-console.log(fullName.length);
+// console.log(fullName.length);
 
 
-// metodos de string
+// // metodos de string
 
-console.log(fullName.toUpperCase());
+// console.log(fullName.toUpperCase());
 
-const result = fullName.toLocaleLowerCase();
+// const result = fullName.toLocaleLowerCase();
 
-console.log(result, fullName);
+// console.log(result, fullName);
 
-const index = email.indexOf('@');
+// const index = email.indexOf('@');
 
-console.log(index);
-*/
+// console.log(index);
+// */
 
 
 
-//AULA 02-04 ****************************************************************************
-// Métodos de strings
-// Não mudam o valor original
+// //AULA 02-04 ****************************************************************************
+// // Métodos de strings
+// // Não mudam o valor original
 
-/*
-const email = 'laracroft@rogermelo.com.br';
+// /*
+// const email = 'laracroft@rogermelo.com.br';
 
-//# lastIndexOf -> Obtem a ultima ocorrencia do caracter passado como argumento
+// //# lastIndexOf -> Obtem a ultima ocorrencia do caracter passado como argumento
 
-const lastIndexOfA = email.lastIndexOf('a');
+// const lastIndexOfA = email.lastIndexOf('a');
 
-console.log(lastIndexOfA);
+// console.log(lastIndexOfA);
 
 
-//# slice -> 'fatiar' a string - 1arg. de onde quer começar, 2arg. onde quer ser cortada.
+// //# slice -> 'fatiar' a string - 1arg. de onde quer começar, 2arg. onde quer ser cortada.
 
-const emailSlice = email.slice(0, 9);
+// const emailSlice = email.slice(0, 9);
 
-console.log(emailSlice); //laracroft
+// console.log(emailSlice); //laracroft
 
 
-//# replace -> subistituir o caracter da string por outro
+// //# replace -> subistituir o caracter da string por outro
 
-const emailReplace = email.replace('l', 'y');
+// const emailReplace = email.replace('l', 'y');
 
-console.log(emailReplace) //yaracroft@rogermelo.com.br
-*/
+// console.log(emailReplace) //yaracroft@rogermelo.com.br
+// */
 
 
 
 
-//AULA 02-05 ****************************************************************************
-// Numbers
+// //AULA 02-05 ****************************************************************************
+// // Numbers
 
-/*
-//inteiros e decimais
-const radius = 10;
-const pi = 3.14;
+// /*
+// //inteiros e decimais
+// const radius = 10;
+// const pi = 3.14;
 
-console.log(radius, pi);
+// console.log(radius, pi);
 
 
-// operadores aritimeticos
-const area = pi * radius ** 2;
+// // operadores aritimeticos
+// const area = pi * radius ** 2;
 
 
-// ordem de operação
+// // ordem de operação
 
-const crazyOperations = 5 * (10 - 3) ** 2;
+// const crazyOperations = 5 * (10 - 3) ** 2;
 
-console.log(crazyOperations);
+// console.log(crazyOperations);
 
-//1ª parênteses
-//2ª expoentes ou raízes
-//3ª multiplicação e divisão
-//4ª adição e subtração
+// //1ª parênteses
+// //2ª expoentes ou raízes
+// //3ª multiplicação e divisão
+// //4ª adição e subtração
 
 
-// operadores de incrementos e decremento
-let postLikes = 10;
+// // operadores de incrementos e decremento
+// let postLikes = 10;
 
-//postLikes = postLikes + 1;
-// postLikes++;
-// postLikes--;
-//console.log(postLikes);
+// //postLikes = postLikes + 1;
+// // postLikes++;
+// // postLikes--;
+// //console.log(postLikes);
 
 
-// operadores addition, substraction, multiplication e division assignment
-//postLikes += 10;
-//postLikes -= 5;
-//postLikes *= 2;
-//postLikes /= 2;
+// // operadores addition, substraction, multiplication e division assignment
+// //postLikes += 10;
+// //postLikes -= 5;
+// //postLikes *= 2;
+// //postLikes /= 2;
 
-//console.log(postLikes);
+// //console.log(postLikes);
 
 
 
-// NaN - not a number
-console.log(7 / 'oi');
+// // NaN - not a number
+// console.log(7 / 'oi');
 
 
-// concatenação de string com número
+// // concatenação de string com número
 
-const likesMessage = 'O post tem ' + postLikes + ' likes.';
+// const likesMessage = 'O post tem ' + postLikes + ' likes.';
 
-console.log(likesMessage);
-*/
+// console.log(likesMessage);
+// */
 
 
 
 
-//AULA 02-06 ****************************************************************************
-// Template strings
+// //AULA 02-06 ****************************************************************************
+// // Template strings
 
-/*
-const postTitle = 'É bolacha ou biscoito?';
-const postAuthor = 'Matheus Saad';
-const postComments = 15;
+// /*
+// const postTitle = 'É bolacha ou biscoito?';
+// const postAuthor = 'Matheus Saad';
+// const postComments = 15;
 
 
-// usando concatenação
+// // usando concatenação
 
 
-// usando template strings
-// const postMessage = `O post "${postTitle}", do ${postAuthor}, tem ${postComments} comentários.`
-// console.log(postMessage);
+// // usando template strings
+// // const postMessage = `O post "${postTitle}", do ${postAuthor}, tem ${postComments} comentários.`
+// // console.log(postMessage);
 
-// cirando templates HTML
+// // cirando templates HTML
 
-const html = `
-    <h2>${postTitle}<h2>
-    <p>Autor: ${postAuthor}</p>
-    <span>Este post tem ${postComments} comentários</span>
-`
-console.log(html);
+// const html = `
+//     <h2>${postTitle}<h2>
+//     <p>Autor: ${postAuthor}</p>
+//     <span>Este post tem ${postComments} comentários</span>
+// `
+// console.log(html);
 
-*/
+// */
 
 
 
 
 
-// AULA 03-02 ****************************************************************************
+// // AULA 03-02 ****************************************************************************
 
-// Arrays
+// // Arrays
 
-// Arrays são utilizados para armazenar uma lista de valores que geralmente tem relação entre si
+// // Arrays são utilizados para armazenar uma lista de valores que geralmente tem relação entre si
 
-let heroes = ['Batman', 'Catwoman', 'Iron Man'];
+// let heroes = ['Batman', 'Catwoman', 'Iron Man'];
 
-//sobrescrever um valor
-heroes[2] = 'Spider-Man';
-//
+// //sobrescrever um valor
+// heroes[2] = 'Spider-Man';
+// //
 
-//const age = [31, 25, 39, 40, 25];
+// //const age = [31, 25, 39, 40, 25];
 
-const randomArray = ['Parker', 'Diana', 19, 18];
+// const randomArray = ['Parker', 'Diana', 19, 18];
 
 
-// Metodos de arrays
-//join -> retorna uma nova string do array concatenado e separado por ',' mas podemos passar um separador
-const joinHeroes = heroes.join(' | ');
+// // Metodos de arrays
+// //join -> retorna uma nova string do array concatenado e separado por ',' mas podemos passar um separador
+// const joinHeroes = heroes.join(' | ');
 
-//indexOf -> obtem o index da primeira ocorrencia do valor passado. Valor passado que não existe no array retorna -1 ****
-//const indexOf25 = age.indexOf(25); 
+// //indexOf -> obtem o index da primeira ocorrencia do valor passado. Valor passado que não existe no array retorna -1 ****
+// //const indexOf25 = age.indexOf(25); 
 
-//concat -> concatena 2 arrays e nao modifica o array original.
-const moreHeroes = heroes.concat(['Superman', 'Wolverine']);
+// //concat -> concatena 2 arrays e nao modifica o array original.
+// const moreHeroes = heroes.concat(['Superman', 'Wolverine']);
 
-//push -> adiciona valores passados ao fim do array e retorna o novo numero de itens do array. ALTERA O VALOR ORIGINAL DO ARRAY
-const pushToHeroes = heroes.push('Cyclops', 'Hulk');
+// //push -> adiciona valores passados ao fim do array e retorna o novo numero de itens do array. ALTERA O VALOR ORIGINAL DO ARRAY
+// const pushToHeroes = heroes.push('Cyclops', 'Hulk');
 
-//pop -> remove o ultimo item de um array e retorna esse item removido
-const popHeroes = heroes.pop('Hulk');
+// //pop -> remove o ultimo item de um array e retorna esse item removido
+// const popHeroes = heroes.pop('Hulk');
 
-// console.log(heroes);
+// // console.log(heroes);
 
 
 
-// AULA 03-03 ****************************************************************************
-//Null e undefined
+// // AULA 03-03 ****************************************************************************
+// //Null e undefined
 
 
 
 
 
-// AULA 04-02 ****************************************************************************
-//Booleans e comparações
-// console.log(true, false, 'true', 'false');
+// // AULA 04-02 ****************************************************************************
+// //Booleans e comparações
+// // console.log(true, false, 'true', 'false');
 
 
-// Métodos podem retornar booleans
-// método includes -> PODE SER USADO EM STRINGS E ARRAYS  -verifica se uma string existe em outra string
-const email = 'brucewayne@renanmaia.com.br';
-const includes = email.includes('wayne');
-//const names = ['dio', 'roger', 'robert'];
-//const arrayIncludes = names.includes('rob');
+// // Métodos podem retornar booleans
+// // método includes -> PODE SER USADO EM STRINGS E ARRAYS  -verifica se uma string existe em outra string
+// const email = 'brucewayne@renanmaia.com.br';
+// const includes = email.includes('wayne');
+// //const names = ['dio', 'roger', 'robert'];
+// //const arrayIncludes = names.includes('rob');
 
-// console.log(includes);
-// console.log(arrayIncludes);
+// // console.log(includes);
+// // console.log(arrayIncludes);
 
 
-//Operadores de comparação
+// //Operadores de comparação
+
+// // const age = 31;
+// const name = 'roger'; //letra minuscula é maior que caracteres MAIUSCULOS
+
+// // console.log(age == 31);
+// // console.log(age == 35);
+// // console.log(age != 35); // age "!= não é igual" a 35
+// // console.log(age != 31);
+// // console.log(age > 30);
+// // console.log(age > 31);
+// // console.log(age < 31);
+// // console.log(age < 32);
+// // console.log(age <= 31);
+// // console.log(age >= 31);
+
+// // console.log(name == 'roger');
+// // console.log(name == 'Roger');
+// // console.log(name > 'belinha');
+// // console.log(name > 'Roger');
+// // console.log(name > 'Belinha');
+
+
+// // AULA 04-03 ****************************************************************************
 
 // const age = 31;
-const name = 'roger'; //letra minuscula é maior que caracteres MAIUSCULOS
 
-// console.log(age == 31);
-// console.log(age == 35);
-// console.log(age != 35); // age "!= não é igual" a 35
-// console.log(age != 31);
-// console.log(age > 30);
-// console.log(age > 31);
-// console.log(age < 31);
-// console.log(age < 32);
-// console.log(age <= 31);
-// console.log(age >= 31);
-
-// console.log(name == 'roger');
-// console.log(name == 'Roger');
-// console.log(name > 'belinha');
-// console.log(name > 'Roger');
-// console.log(name > 'Belinha');
+// // 'igual a' e 'diferente de'
+// // console.log(age == 31);
+// // console.log(age == '31'); //a string '31' é convertida para number devido ao sinal '=='
+// // console.log(age != 31);
+// // console.log(age != '31');//a string '31' é convertida para number devido ao sinal '=='
 
 
-// AULA 04-03 ****************************************************************************
+// // 'igual a, e do mesmo tipo' e 'diferente de, e do mesmo tipo'
 
-const age = 31;
-
-// 'igual a' e 'diferente de'
-// console.log(age == 31);
-// console.log(age == '31'); //a string '31' é convertida para number devido ao sinal '=='
-// console.log(age != 31);
-// console.log(age != '31');//a string '31' é convertida para number devido ao sinal '=='
-
-
-// 'igual a, e do mesmo tipo' e 'diferente de, e do mesmo tipo'
-
-// console.log(age === 31);
-// console.log(age === '31');
-// console.log(age !== 31);
-// console.log(age !== '31');
+// // console.log(age === 31);
+// // console.log(age === '31');
+// // console.log(age !== 31);
+// // console.log(age !== '31');
 
 
 
 
 
-// AULA 04-04 ****************************************************************************
-//Conversao de tipos
+// // AULA 04-04 ****************************************************************************
+// //Conversao de tipos
 
-let score = '100';
+// let score = '100';
 
-//score = Number(score); //converte string para NUMBER
+// //score = Number(score); //converte string para NUMBER
 
-// utilizamos typreof para identificar o tipo de dado
-// console.log(score + 1);
-// console.log(typeof score);
+// // utilizamos typreof para identificar o tipo de dado
+// // console.log(score + 1);
+// // console.log(typeof score);
 
-const crazyConversion = Number('Maça');
-const convertedNumber = String(97);
-const booleanConversion = Boolean(10);
+// const crazyConversion = Number('Maça');
+// const convertedNumber = String(97);
+// const booleanConversion = Boolean(10);
 
-// console.log(crazyConversion);
-// console.log(convertedNumber, typeof convertedNumber); //""
-// console.log(booleanConversion);
+// // console.log(crazyConversion);
+// // console.log(convertedNumber, typeof convertedNumber); //""
+// // console.log(booleanConversion);
 
-/* 
-    Valores falsy:
-    - false
-    - 0
-    - "", '', ``
-    - null
-    - undefined
-    - NaN
+// /* 
+//     Valores falsy:
+//     - false
+//     - 0
+//     - "", '', ``
+//     - null
+//     - undefined
+//     - NaN
 
-    Valores truthy:
-    - Qualquer valor que nao é falsy, ou seja, qualquer valor que não esteja aprensentado no falsy a cima
-*/
+//     Valores truthy:
+//     - Qualquer valor que nao é falsy, ou seja, qualquer valor que não esteja aprensentado no falsy a cima
+// */
 
 
 
 
 
 
-/// ETAPA 2 CONTROLE DE FLUXO ***********************************************************************************************************
+// /// ETAPA 2 CONTROLE DE FLUXO ***********************************************************************************************************
 
-/// AULA 01-03
-/// For loop  *************************************************************************************************************
+// /// AULA 01-03
+// /// For loop  *************************************************************************************************************
 
 
-// for (let i = 0; i < 5; i++) {
-//     console.log(`Dentro do loop: ${i}`);
+// // for (let i = 0; i < 5; i++) {
+// //     console.log(`Dentro do loop: ${i}`);
+// // }
+
+// // console.log('Loop concluido');
+
+
+
+// // const names = ['Linus', 'Ada', 'Bill'];
+// //                 //resulta em 3 que é a quantidade de itens do array, isso nos assegura que vamos executar o codigo baseado na quantidade
+// //                 //de itens do array sem saber quantos itens possui.
+// // for (let i = 0; i < names.length; i++) {
+// //     //console.log(i) //0, 1, 2
+// //     console.log(names[i]);
+// // }
+
+
+// // for (let i = 0; i < names.length; i++) {
+// //     const HTMLTemplate = `<p>${names[i]}</p>`
+// //     console.log(HTMLTemplate);
+// // }
+
+
+
+
+
+// // / AULA 01-04
+// // / While loop  *************************************************************************************************************
+
+
+// // let i = 0
+
+// // while(i < 5) {
+// //     console.log(`Dentro do loop while: ${i}`);
+// //     i++;
+// // }
+
+
+// // const names = ['Lívia', 'Renan', 'Tita', 'Brisa'];
+// // let i = 0;
+
+// // while(i < names.length) {
+// //     console.log(names[i]); //names[i] i terá o valor dos indixes dos arrays, ex: começa com i = 0, 0 é o primeiro index do array;
+// //     i++;
+// // }
+
+
+
+
+
+
+// // / AULA 02-02
+// // A Condicional if ***********************************************************************************************
+
+// // if (se) se uma condição for verdadeira fazer algo, só executa se for TRUE.
+
+
+
+// const age = 19;
+
+// if (age > 18) {
+//     console.log('Você é maior de idade');
 // }
 
-// console.log('Loop concluido');
 
+// const simpsons = ['Marge', 'Homer', 'Lisa', 'Bart'];
 
-
-// const names = ['Linus', 'Ada', 'Bill'];
-//                 //resulta em 3 que é a quantidade de itens do array, isso nos assegura que vamos executar o codigo baseado na quantidade
-//                 //de itens do array sem saber quantos itens possui.
-// for (let i = 0; i < names.length; i++) {
-//     //console.log(i) //0, 1, 2
-//     console.log(names[i]);
+// if(simpsons.length >= 5) {
+//     console.log('O array tem bastante personagens');
 // }
 
 
-// for (let i = 0; i < names.length; i++) {
-//     const HTMLTemplate = `<p>${names[i]}</p>`
-//     console.log(HTMLTemplate);
+// const password = 'oi123321';
+
+// if (password.length >= 8) {
+//     console.log('Essa senha tem 8 ou mais caracteres');
 // }
 
 
+// AULA 02-03
+// Else e else if statements **********************************************************************
 
 
+const password = 'oi123';
 
-/// AULA 01-04
-/// While loop  *************************************************************************************************************
-
-
-// let i = 0
-
-// while(i < 5) {
-//     console.log(`Dentro do loop while: ${i}`);
-//     i++;
-// }
-
-
-// const names = ['Lívia', 'Renan', 'Tita', 'Brisa'];
-// let i = 0;
-
-// while(i < names.length) {
-//     console.log(names[i]); //names[i] i terá o valor dos indixes dos arrays, ex: começa com i = 0, 0 é o primeiro index do array;
-//     i++;
-// }
+if (password.length >= 8) {
+    console.log('Essa senha tem 8 ou mais caracteres');
+}
