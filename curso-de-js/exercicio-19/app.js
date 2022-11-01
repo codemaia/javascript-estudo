f0d99f6b059508cfd6c11dd35d8ae52a5f4f3388
Renan Maia <renanmaia.rl@gmail.com>
2022-10-31 22:59:46 -0300

    creating quiz

--- curso-de-js/exercicio-19/app.js
+++ curso-de-js/exercicio-19/app.js
@@ -13,4 +13,41 @@ Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, bus
 */
 
 
+const correctAnswer = ['C', 'C', 'C', 'C'];
 
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let scoreUser = 0;
+
+    const answerUser = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value
+    ];
+
+    answerUser.forEach((answer, index) => {
+        
+        if (answer === correctAnswer[index]) {
+            scoreUser += 25;
+        }
+
+    });
+
+    if (scoreUser === 0) {
+        alert(`Calma jovem, tenta de novo <3! Você tirou ${scoreUser}, mas não significa nada! =)`);
+    } else if (scoreUser === 25) {
+        alert(`Boa, ${scoreUser}% do quiz! mas da pra melhorar!`);
+    } else if (scoreUser === 50) {
+        alert(`Muito bom, ${scoreUser}% do quiz! Vamo que da pra fechar o quiz!`);
+    } else if (scoreUser === 75) {
+        alert(`Show, acertou ${scoreUser}%! Quase perfeito hein?! Vamo acertar tudo agora?`);
+    } else {
+        alert(`Perfeito! você acertou ${scoreUser}% do quiz`)
+    }
+
+});
