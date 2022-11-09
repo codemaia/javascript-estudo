60c794cd04166d567954b6f1475114cdff34f116
Renan Maia <renanmaia.rl@gmail.com>
2022-11-08 23:03:23 -0300

    refactoring quizz

--- quiz-interativo-master/app.js
+++ quiz-interativo-master/app.js
@@ -3,8 +3,12 @@ const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('form');
 const finalResult = document.querySelector('.result');
 
+let score = 0;
+let counter = 0;
+let timer;
 
 const getUserAnswers = () => {
+
     const userAnswers = [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
@@ -16,16 +20,8 @@ const getUserAnswers = () => {
 };
 
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    
-    let score = 0;
-    
-// OBTEM A RESPOSTA DO USUARIO
-    const userAnswers = getUserAnswers();
-
-
-// CALCULA A PONTUAÇÃO DO USUARIO
+    /** ira pegar o userAnswers como parametro, que esta dentro do form.addEventListener */
+const calculateUserScore = userAnswers => {
     userAnswers.forEach((answer, index) => {
     
         if (answer === correctAnswers[index]) {
@@ -33,21 +29,19 @@ form.addEventListener('submit', event => {
         }
         
     });
+};
 
 
-
-// IR PARA O INICIAR
+const showScore = () => {
     scrollTo(0, 0);
     finalResult.classList.remove('d-none');
-    
+};  
 
-// ANIMA O RESULTADO
 
-    let counter = 0;
-    
+const animationResult = () => {
     // armazena na const timer para poder pegar o ID que é gerado cada vez que um setInterval é criado.
     // com esse ID podemos parar o setInterval;
-    const timer = setInterval(() => {
+    timer = setInterval(() => {
         //parar o setInterval quando o counter for igual ao score acertado;
         //passa o timer para o clearInterval pois nele cosnta o ID do setInterval
         
@@ -55,10 +49,30 @@ form.addEventListener('submit', event => {
             clearInterval(timer);
         }
         
-        finalResult.querySelector('span').textContent = `${counter}%`;
-        counter++ //necessário incrementar o counter para o valor poder mudar.
+        finalResult.querySelector('span').textContent = `${counter++}%`;
+                     //necessário incrementar o counter para o valor poder mudar.
         
     }, 10);
+};
+
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    
+// OBTEM A RESPOSTA DO USUARIO
+    const userAnswers = getUserAnswers();
+
+// CALCULA A PONTUAÇÃO DO USUARIO
+    calculateUserScore(userAnswers);
+
+
+// EXIBE A PONTUAÇÃO
+    showScore();
+    
+// ANIMA O RESULTADO
+    animationResult();
 
 });
 
