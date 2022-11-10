9b1e4eb5632e61feef6611c3f83e910ab428d68b
Renan Maia <renanmaia.rl@gmail.com>
2022-11-09 22:24:29 -0300

    GOD IS GOOD!

--- app.js
+++ app.js
@@ -2107,3 +2107,81 @@ const article = document.querySelector('article');
  *      clearInterval(AQUI VAI O ID DO SETINTERVAL);
  * 
  */
+
+
+
+
+
+
+
+
+// /// ETAPA 8 MÉTODOS DE ARRAY ********MAP***FILTER****REDUCE********************************************************************************************
+
+
+//# AULA 01-02 - O MÉTODO MAP
+/**
+ *              #IMPORTANTE
+ *   # NÃO MODIFICA O ARRAY ORIGINAL
+ *   # PRECISA SEMPRE RETORNAR UM VALOR 
+ *   # GERAR UM NOVO ARRAY COM A MESMA QUANTIDADE DE ITENS DO ARRAY ORIGINAL SO QUE COM ELEMENTOS TRANSFORMADOS POR UMA FUNÇÃO
+ * 
+ * 
+ */
+
+
+// @ MAP!
+
+// const numbers = [1, 2, 3];
+
+// // const doubleNumbers = numbers.map((item /*, index, array */) => {
+// //     return item * 2;
+// // });
+//  const doubleNumbers = numbers.map(item => item * 2);
+
+
+// console.log(doubleNumbers, numbers);
+
+
+
+// const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+
+// const salePrices = prices.map(price => price / 2);
+
+// console.log(salePrices);
+
+
+
+
+const products = [
+    { name: 'Mouse Sem Fio', price: 30 },
+    { name: 'Pen Drive', price: 25 },
+    { name: 'Cartucho de Tinta', price: 50 },
+    { name: 'Suporte Ergonômico para Notebook', price: 23 },
+    { name: 'Repetidor de Sinal Wi-Fi', price: 44 }
+];
+
+// DESTE MODO MODIFICAMOS O ARRAY ORIGINAL
+// const saleProducts = products.map(product => {
+    
+//     if (product.price >= 30) {
+//         product.price = product.price / 2; // DESTE MODO MODIFICAMOS O ARRAY ORIGINAL
+//         return product
+//     }
+
+//     return product
+// });
+
+                                    //aqui recebemos o proprio objeto
+const saleProducts = products.map(product => {
+    if (product.price >= 30) {
+        return {
+            name: product.name, price: product.price / 2
+        }
+    }
+
+    return product;
+});
+
+console.log(saleProducts, products)
+
+
