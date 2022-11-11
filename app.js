f26886b212e5b8b458f1d28db1fc96d75b55b7c0
Renan Maia <renanmaia.rl@gmail.com>
2022-11-11 16:51:12 -0300

    GOD IS GOOD!

--- app.js
+++ app.js
@@ -2373,3 +2373,17 @@ scores.sort((score1, score2) => score1 - score2);
 console.log(scores);
 
 // exemplo 3: ordenando objetos
+
+
+const theBigFamily = [
+    {name: 'Lineu', score: 20},
+    {name: 'Nenê', score: 10},
+    {name: 'Tuco', score: 50},
+    {name: 'Bebel', score: 30},
+    {name: 'Agostinho', score: 70}
+];
+
+
+theBigFamily.sort((item1, item2) => item2.score - item1.score);
+
+console.log(theBigFamily);
