70f710b365327cedc2c537bbd9eaae9829e3e7f3
Renan Maia <renanmaia.rl@gmail.com>
2022-11-19 19:38:40 -0300

    add to do and clean input

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,16 +1,26 @@
 const formTodo = document.querySelector('.form-add-todo');
 const todosContainer = document.querySelector('.todos-container');
 
+
+
 formTodo.addEventListener('submit', event => {
     event.preventDefault();
 
     const inputValue = event.target.add.value.trim();
 
-    todosContainer.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${inputValue}</span>
-            <i class="far fa-trash-alt delete"></i>
-        </li>
-        `
+    if (inputValue) {
+        todosContainer.innerHTML += `
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${inputValue}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+            `
+        
+        formTodo.add.value = ''
+
+    }
+
+
+
     
 });
