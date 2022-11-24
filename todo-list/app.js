72780511814c1919d9484e37892892a3abe69aa0
Renan Maia <renanmaia.rl@gmail.com>
2022-11-23 23:00:37 -0300

    adding the feature to search, but have in many bugs

--- todo-list/app.js
+++ todo-list/app.js
@@ -39,17 +39,29 @@ todosContainer.addEventListener('click', event => {
 
 formSearch.addEventListener('input', event => {
     
-    const inputValue = event.target.value.toLowerCase().trim();
+    const inputValue = event.target.value.trim();
     // console.log(inputValue);
     // console.log(todosContainer.textContent.toLowerCase().trim().includes(inputValue));
-   
-    if (!todosContainer.textContent.toLowerCase().includes(inputValue)) {
-        // console.log(Array.from(todosContainer.children));
-        Array.from(todosContainer.children).forEach(li => {
-            console.log(li);
-        });
+    // console.log(!todosContainer.textContent.toLowerCase().trim().includes(inputValue))
 
-    }
+    Array.from(todosContainer.children)
+        .filter(todo => {
+           return !todo.textContent.includes(inputValue);
+    }).forEach(todo => {
+        todo.classList.add('hidden');
+        todo.classList.remove('d-flex');
+    });
+
+    Array.from(todosContainer.children)
+        .filter(todo => {
+           return todo.textContent.includes(inputValue); // com o filter recebe todas as <lis> que não possui o valor da busca
+    }).forEach(todo => { // add a classe nas <li> que não correspondem a busca  
+        todo.classList.add('d-flex');
+        todo.classList.remove('hidden');
+    });
+
     
+   
+
 
 });
