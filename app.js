0f97fbb8db81ab1e1db475fc1cfa50172359f54a
Renan Maia <renanmaia.rl@gmail.com>
2022-11-27 18:49:12 -0300

    exercise 26. God is good!

--- app.js
+++ app.js
@@ -2603,13 +2603,43 @@ const present = new Date();
 // const difference = present2.getTime() - past.getTime();
 
 // const seconds = Math.round(difference / 1000);
-// const minutes = Math.round(seconds / 60);
+// const minutes = Math.round(seconds / 60);b
 // const hours = Math.round(minutes / 60)
 
 // const days = Math.round(hours / 24);
 
 // console.log(difference);
 // console.log(days)
+
+
+
+
+
+
+
+
+// AULA 02-03 CONHECENDO DATA-FNS
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+const present3 =  new Date();
+
+
+console.log(dateFns);
+console.log(dateFns.isToday(present3)) //verificar a data atual
+
+
+console.log(dateFns.format(present3, 'DD/MM/YYYY'))
+
+
+const past2 = new Date ('Nov 27 2022 17:00:00');
+
+console.log(dateFns.distanceInWords(present3, past2,{ addSuffix: true })) //retorna quanto tempo se passou até a data atual.
 
 
 
