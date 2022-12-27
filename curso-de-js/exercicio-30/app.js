c1eebe0d92c257391029cc077466b534caf9fc50
Renan Maia <renanmaia.rl@gmail.com>
2022-12-27 10:21:01 -0300

    I'm getting the hang of javascript. GOD IS GOOD!

--- curso-de-js/exercicio-30/app.js
+++ curso-de-js/exercicio-30/app.js
@@ -225,9 +225,9 @@ console.log(newBrasilTwo)
 
 const newBrasilTwoWithIdPair = newBrasilTwo.filter(({ id }) => id % 2 === 0);
 
-console.log(newBrasilTwoWithIdPair);
-console.log(newBrasilTwoWithIdPair);
-console.log(newBrasilTwoWithIdPair);
+// console.log(newBrasilTwoWithIdPair);
+// console.log(newBrasilTwoWithIdPair);
+// console.log(newBrasilTwoWithIdPair);
 
 
 
